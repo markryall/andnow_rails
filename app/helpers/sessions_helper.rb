5ebe6e2364868d2e39561d51eb42ee2016fe1951
Mark Ryall <mark@ryall.name>
2012-02-06 09:08:15 +1000

    added duration to session index and formatted start and end as time

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,14 @@
 module SessionsHelper
+  def time ms
+    Time.at(ms/1000).strftime '%d/%m %H:%M'
+  end
+
+  def duration ms
+    s,ms=ms/1000,ms%1000
+    minutes,s=s/60,s%60
+    hours,minutes=minutes/60,minutes%60
+    return "#{hours} hours #{minutes} minutes" if hours > 0
+    return "#{minutes} minutes #{s} seconds" if minutes > 0
+    "#{s}.#{ms} seconds"
+  end
 end
