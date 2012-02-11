1c880f0601d92fbe693209776a3d56a93e488910
Mark Ryall <mark@ryall.name>
2012-02-11 13:00:35 +1000

    timestamps are now seconds instead of millis

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,18 +1,17 @@
 module SessionsHelper
-  def time ms
+  def time s
     return '' unless ms
-    Time.at(ms/1000).strftime '%a %d %b %H:%M:%S'
+    Time.at(s).strftime '%a %d %b %H:%M:%S'
   end
 
   def duration from, to
     return '' unless from and to
-    ms = to - from
-    s,ms=ms/1000,ms%1000
+    s = to - from
     minutes,s=s/60,s%60
     hours,minutes=minutes/60,minutes%60
     return "#{hours} hours #{minutes} minutes" if hours > 0
     return "#{minutes} minutes #{s} seconds" if minutes > 0
-    "#{s}.#{ms} seconds"
+    "#{s} seconds"
   end
 
   def cost c
